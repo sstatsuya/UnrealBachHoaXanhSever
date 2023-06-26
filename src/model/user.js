c7cca10185adbca0ea896a704e00ce090bd6082a
@193111 <tienluongminh@thegioididong.com>
2023-06-26 21:17:28 +0700

    Add register user api

--- src/model/user.js
+++ src/model/user.js
@@ -1,5 +1,12 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const User =  new Schema({
-})
-module.exports = mongoose.model('users', User)
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const User = new Schema({
+  userID: String,
+  username: String,
+  avatar: String,
+  password: String,
+  fullName: String,
+  phoneNumber: String,
+  email: String,
+});
+module.exports = mongoose.model("users", User);
--- src/route/user.js
+++ src/route/user.js
@@ -1,16 +1,34 @@
-const express = require("express"); 
-const router = express.Router()
-const User = require('../model/user') // import file model ở trên vào
-router.get('/', (req, res)=>{
-    res.send("dday la trang User")
-})
-router.get("/all", async (req, res) => {   
-    try{
-        const data = await User.find({})
-        res.json(data)
-    } 
-    catch(err){
-        res.status(500).json({error: 'Error when get Users'})
-    }
+const express = require("express");
+const router = express.Router();
+const User = require("../model/user"); // import file model ở trên vào
+router.get("/", (req, res) => {
+  res.send("dday la trang User");
 });
-module.exports = router
+router.get("/all", async (req, res) => {
+  try {
+    const data = await User.find({});
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: "Error when get Users" });
+  }
+});
+
+router.post("/register", async (req, res) => {
+  try {
+    console.log(req.body);
+    const newUser = new User({
+      userID: req.body.userID,
+      username: req.body.username,
+      avatar: req.body.avatar,
+      password: req.body.password,
+      fullName: req.body.fullName,
+      phoneNumber: req.body.phoneNumber,
+      email: req.body.email,
+    });
+    await newUser.save();
+    res.json(newUser);
+  } catch (error) {
+    res.status(500).json({ error: "Error when register" });
+  }
+});
+module.exports = router;
